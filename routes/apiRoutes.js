5ddeb12c7a5ba4523d6b6e61cf685a64f73d79a4
Jessica Galarza <jessicaga941@gmail.com>
2021-10-26 22:05:13 -0400

    edited and updated apiRoutes.js folder

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,13 +1,35 @@
-const path=require('path');
-const router=require('express').Router();
+const notes = require('express').Router()
+const {readFromFile, readAndAppend, deleteNote} = require('../helpers/fsUtils');
+const uuid = require('../helpers/uuid');
+
+notes.get('/api/notes', (req, res) => {
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
 
 
-router.get('/notes', (req, res)=>{
+notes.post('/api/notes', (req, res) => {
+  console.log(req.body);
 
+  const { title, text} = req.body;
 
+  if (req.body) {
+    const newNote = {
+      title,
+      text,
+      id: uuid(),
+    };
+  
+    readAndAppend(newNote, './db/db.json');
+    res.json(`Note added successfully 🚀`);
+  } else {
+    res.json('Error in adding note');
+  }
+});
+
+notes.delete('/api/notes/:id', (req, res) => {
+  console.log(req.params.id);
+  deleteNote(req.params.id, './db/db.json');
+  res.json('success')
 })
-router.post('/notes', (req, res)=>{
-    res.sendFile(path.join(__dirname, '../public/notes.html'));
 
-})
-module.exports = router;
+module.exports = notes;
