23790885b35025b7d1879f89692227ab6a420767
Jessica Galarza <jessicaga941@gmail.com>
2021-10-31 14:17:53 -0400

    initial commit

---
+++ routes/notes.js
@@ -0,0 +1,41 @@
+const notes = require('express').Router();
+const { readFromFile, readAndAppend, readAndDelete } = require('../helpers/fsUtils');
+const uuid = require('../helpers/uuid');
+
+
+// GET Route for retrieving all the tips
+notes.get('/', (req, res) => {
+  console.info(`${req.method} request received for notes`);
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+// POST Route for a new UX/UI tip
+notes.post('/', (req, res) => {
+  console.info(`${req.method} request received to add a note`);
+  console.log(req.body);
+
+  const { title, text} = req.body;
+
+  if (req.body) {
+    const newNote = {
+      id: uuid(),
+      title,
+      text
+    };
+
+    readAndAppend(newNote, './db/db.json');
+    res.json(`Note added successfully 🚀`);
+  } else {
+    res.error('Error in adding note');
+  }
+});
+
+notes.delete('/', (req, res) => {
+    console.info(`${req.method} request received to delete a note`);
+      console.log("id: " + req.query.id);
+      readAndDelete(req.query.id, './db/db.json');
+      res.json(`Note deleted successfully`);
+
+  });
+
+module.exports = notes;
---
+++ routes/router.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+
+const notesRouter = require('./notes.js');
+
+const app = express();
+
+app.use('/notes', notesRouter);
+
+module.exports = app;
